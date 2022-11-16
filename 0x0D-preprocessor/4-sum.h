09bd5a1ea63014c240d5b407f3ba1ac184503143
AlvineMoses <65952407+AlvineMoses@users.noreply.github.com>
2022-11-16 17:54:31 +0300

    Update 4-sum.h

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,13 +1,6 @@
 #ifndef SUM_H
 #define SUM_H
 
-/*
- * JIGJIGA, ETHIOPIA
- * Auth: Ismail Mohamed Ismail
- * Desc: Header file that defines a function-like macro SUM(x, y)
- *       that computes the sum of the numbers x and y.
- */
-
 #define SUM(x, y) ((x) + (y))
 
 #endif
